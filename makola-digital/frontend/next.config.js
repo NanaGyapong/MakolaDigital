28971f5aaa34b721af8d6f728b2d3aadf15ecbd3
Bismark Ababio <nanababio18@gmail.com>
2026-05-24 19:13:52 +0000

    fix: remove standalone, use npm start

--- makola-digital/frontend/next.config.js
+++ makola-digital/frontend/next.config.js
@@ -1,8 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: undefined,
-  }
-}
+const nextConfig = {}
 module.exports = nextConfig
