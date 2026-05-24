01562e65f84b51307a3e11de48fa6bf093b64e30
Bismark Ababio <nanababio18@gmail.com>
2026-05-24 05:58:10 +0000

    fix: next.config for Render standalone build

--- makola-digital/frontend/next.config.js
+++ makola-digital/frontend/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined,
+  }
 }
 module.exports = nextConfig
