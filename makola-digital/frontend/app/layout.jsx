82ef50b4222d5cb216a37870a86d2875499a6bb6
Bismark Ababio <nanababio18@gmail.com>
2026-06-27 12:52:48 +0000

    feat: add Google Analytics G-VDFV4N6FRE

--- makola-digital/frontend/app/layout.jsx
+++ makola-digital/frontend/app/layout.jsx
@@ -58,6 +58,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VDFV4N6FRE"></script>
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-VDFV4N6FRE');` }} />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#E8533A" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
