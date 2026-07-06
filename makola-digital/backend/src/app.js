8bc0ac0b67b2f74eb8ed5b149bb896834eb70c2f
Bismark Ababio <nanababio18@gmail.com>
2026-07-06 21:58:55 +0000

    fix: add express-rate-limit import

--- makola-digital/backend/src/app.js
+++ makola-digital/backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 import { sendWeeklyAnalytics } from "./jobs/weekly-analytics.js";
 import pushRoutes from "./routes/push.routes.js";
 import { sendBuyerRecommendations } from "./jobs/buyer-recommendations.js";
